d1c065202fae459b1098a5483269e375bdf499e8
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2024-01-14 02:32:49 +0900

    Add files via upload

---
+++ base/start/CommentJava.java
@@ -0,0 +1,15 @@
+package start;
+
+public class CommentJava {
+    public static void main(String[] args){
+        System.out.println("hello java1"); // hello java1 을 콘솔창에 출력
+//        System.out.println("hello java2");
+        System.out.println("hello java3");
+
+        /*
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+         */
+    }
+}
---
+++ base/start/HelloJava.java
@@ -0,0 +1,10 @@
+package start;
+
+// 파일명 == 클래스명
+public class HelloJava {
+    //   프로그램 실행 시작 시점
+    public static void main(String[] args){
+        System.out.println("hello java");
+    }
+
+}
