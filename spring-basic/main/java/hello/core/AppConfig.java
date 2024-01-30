8de568c53900879e71cb6e2c3df434b3bc126d82
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2024-01-31 08:39:28 +0900

    apply spring

--- spring-basic/main/java/hello/core/AppConfig.java
+++ spring-basic/main/java/hello/core/AppConfig.java
@@ -8,21 +8,27 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import hello.core.repository.MemberRepository;
 import hello.core.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
     // 생성자 주입 ! -> 이걸 통해서 DIP 를 지킬 수 있음 구현객체를 이놈이 생성하는거지
+    @Bean
     public MemberService memberService(){
-       return new MemberServiceImpl(memberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 
--- spring-basic/main/java/hello/core/OrderApp.java
+++ spring-basic/main/java/hello/core/OrderApp.java
@@ -5,13 +5,22 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
 
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+        /*Spring 적용 전*/
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        /*Spring 적용 후*/
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
 
         Long memberId = 1L;
