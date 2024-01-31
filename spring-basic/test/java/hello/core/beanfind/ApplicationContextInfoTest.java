6dc4244a48ad1a644ad951a1a8fcaec7065e4fb3
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2024-01-31 09:56:46 +0900

    create a test file that find all bean

---
+++ spring-basic/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,43 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ApplicationContextInfoTest {
+    AnnotationConfigApplicationContext ac =
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든 Bean 출력하기")
+    void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.print("name(key) = " + beanDefinitionName);
+            System.out.println(" / object(value) = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("애플리케이션 Bean 출력하기")
+    void findApplicationAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition =
+                    ac.getBeanDefinition(beanDefinitionName);
+            //BeanDefinition.ROLE_APPLICATION : 직접 등록한 애플리케이션 빈
+            //BeanDefinition.ROLE_INFRASTRUCTURE : 스프링이 내부에서 사용하는 빈
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.print("name(key) = " + beanDefinitionName);
+                System.out.println(" / object(value) = " + bean);
+            }
+        }
+    }
+}
