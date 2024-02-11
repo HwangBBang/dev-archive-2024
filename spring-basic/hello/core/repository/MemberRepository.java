fe711ee95f0fe12945fc68f44325a12f9d86ebdb
HwangBBang <96881651+HwangBBang@users.noreply.github.com>
2024-02-11 23:28:40 +0900

    Delete spring-basic/hello/core directory

--- spring-basic/hello/core/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package hello.core.repository;
-
-import hello.core.member.Member;
-
-public interface MemberRepository {
-    void save(Member member) ;
-    // 회원 ID로 찾기
-    Member findById(Long memberId);
-}
