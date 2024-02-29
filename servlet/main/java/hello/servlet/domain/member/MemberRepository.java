fd5238162c8f746fbf268bc900372526af38a114
HwangBBang <h1009218@hufs.ac.kr>
2024-03-01 00:08:48 +0900

    Revert "about controller,model,view"
    This reverts commit ccd8888dfb73a6b89a4838851740acbee039cf9a.

--- servlet/main/java/hello/servlet/domain/member/MemberRepository.java
+++ servlet/main/java/hello/servlet/domain/member/MemberRepository.java
@@ -21,7 +21,6 @@ public class MemberRepository {
     }
 
     public Member save(Member member) {
-
         member.setId(++sequence);
         store.put(member.getId(), member);
         return member;
