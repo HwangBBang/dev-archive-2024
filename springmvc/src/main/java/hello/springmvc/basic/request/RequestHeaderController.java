02d5fa2954fa004772fe7b0fceb2b2fc40cc24c2
HwangBBang <h1009218@hufs.ac.kr>
2024-03-02 23:19:32 +0900

    create controller about Request

---
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -0,0 +1,39 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMethod;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@Slf4j // log 찍기
+@RestController // view를 찾지않고, 문자 그대로 응답으로 반환
+public class RequestHeaderController {
+
+    @RequestMapping("/headers")
+    public String headers(HttpServletRequest request,
+                          HttpServletResponse response,
+                          HttpMethod httpMethod,
+                          Locale locale,
+                          @RequestHeader MultiValueMap<String, String> headerMap, // header 한번에 다 받기
+                          @RequestHeader("host") String host,                     // header 한개 받기
+                          @CookieValue(value = "myCookie", required = false) String cookie) {
+
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap);
+        log.info("header host={}", host);
+        log.info("myCookie={}", cookie);
+
+        return "ok";
+    }
+}
---
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,80 @@
+package hello.springmvc.basic.request;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        log.info("username={}, age={}",username,age);
+
+        response.getWriter().write("ok");
+    }
+
+    @ResponseBody // RestController와 같은 효과
+    // 위 클래스가 Controller 클래스이기에 반환하는 return String으로 된 view 를 찾는다
+    // 따라서 위 클래스를 RestController 로 바꿔주거나 @ResponseBody 를 붙여주어 응답 바디에 메시지를 띄우게한다.
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("username") String memberName,
+            @RequestParam("age") int memberAge ) {
+        log.info("username={}, age={}",memberName,memberAge);
+        return "OK";
+    }
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String username, // @RequestParam("username") String memberName,
+            @RequestParam int age ) {      // @RequestParam("age") int memberAge,
+        log.info("username={}, age={}",username,age);
+        return "OK";
+    }
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(String username, int age ) {
+        log.info("username={}, age={}",username,age);
+        return "OK";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-required")
+    public String requestParamRequired(
+            @RequestParam(required = false) String username, //required = true 가 default
+            @RequestParam(required = false) int age ) {
+        log.info("username={}, age={}",username,age);
+        return "OK";
+    }
+    @ResponseBody
+    @RequestMapping("/request-param-default")
+    public String requestParamDefault(
+            @RequestParam(required = true , defaultValue = "guest") String username, //required = true 가 default
+            @RequestParam(required = false) int age ) {
+        log.info("username={}, age={}",username,age);
+        return "OK";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-map")
+    public String requestParamMap(@RequestParam Map<String,Object> paramMap ){
+        Object username = paramMap.get("username");
+        Object age = paramMap.get("age");
+
+        log.info("username={}, age={}",username,age);
+        return "OK";
+    }
+
+}
