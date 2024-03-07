9c0c0120259f47e60be5825108f60348a729325f
HwangBBang <h1009218@hufs.ac.kr>
2024-03-07 19:34:53 +0900

    Update BasicItemController.java

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,10 +6,8 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -38,18 +36,80 @@ public class BasicItemController {
     public String addForm() {
         return "basic/addForm";
     }
-    @PostMapping("/add")
-    public String save() {
+    //itemName=itemA&price=10000&quantity=10
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam Integer price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+        model.addAttribute("item",item);
+
         return "basic/addForm";
     }
 
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item ,Model model) { // 모델에트리뷰트를 사용하면 객체생성부터 객체 값 세팅 까지 알아서해줌
+//  < 모델 에트리뷰트가 해줌 >
+//        Item item = new Item();
+//        item.setItemName(itemName);
+//        item.setPrice(price);
+//        item.setQuantity(quantity);
+        itemRepository.save(item);
+        model.addAttribute("item",item);
+
+
+        return "basic/addForm";
+    }
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item ) { // 모델에트리뷰트를 사용하면 객체생성부터 객체 값 세팅 까지 알아서해줌
+        itemRepository.save(item);
+        return "basic/items/";
+    }
+//    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute("item") Item item ,Model model) { // 모델에트리뷰트를 사용하면 객체생성부터 객체 값 세팅 까지 알아서해줌
+
+        itemRepository.save(item);
+        model.addAttribute("item",item);
+
+
+        return "redirect:/basic/items/" +item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(@ModelAttribute("item") Item item , Model model, RedirectAttributes redirectAttributes) { // 모델에트리뷰트를 사용하면 객체생성부터 객체 값 세팅 까지 알아서해줌
+
+        Item savedItem = itemRepository.save(item);
+        model.addAttribute("item",item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
+        return "redirect:/basic/items/{itemId}";
+    }
+
     @GetMapping("/{itemId}/edit")
-    public String itmeEdit(@PathVariable long itemId, Model model) {
+    public String editEdit(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
 
-        return "basic/item";
+        return "basic/editForm";
     }
+    @PostMapping("/{itemId}/edit")
+    public String Edit(@PathVariable long itemId, @ModelAttribute Item item, RedirectAttributes redirectAttributes ) {
+        itemRepository.update(itemId, item);
+        redirectAttributes.addAttribute("itemId", item.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}"; // 리다이렉트
+    }
+
+
+
+
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
